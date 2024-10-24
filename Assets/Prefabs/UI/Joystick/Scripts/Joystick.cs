6c484511f34336587a7128be5a78de1ae87b7620
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-10-24 14:06:57 +0200

    thumbStick limit to the clamp of the background radius

--- Assets/Prefabs/UI/Joystick/Scripts/Joystick.cs
+++ Assets/Prefabs/UI/Joystick/Scripts/Joystick.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class Joystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    //transform of the thumbStick
+    [SerializeField] RectTransform ThumbStickTrans;
+    [SerializeField] RectTransform BackgroundTrans;
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log($"On Drag Fired {eventData.position}");
+
+        //calculate the position the touch is right now
+        Vector2 TouchPos = eventData.position;
+        //the center position of the background
+        Vector2 centerPos = BackgroundTrans.position;
+
+        //The difference between the TouchPos and centerPos is clamped using Vector2.ClampMagnitude,
+        //restricting the offset to the radius of the background (BackgroundTrans.sizeDelta.x / 2).
+        //This ensures that the thumbstick stays within the circular boundary.
+        Vector2 localOffset = Vector2.ClampMagnitude(TouchPos - centerPos, BackgroundTrans.sizeDelta.x/2);
+
+        //moving thumbStickTrans with maximum the radius position of the background, it limits the Vector to the radius.
+        ThumbStickTrans.position = centerPos + localOffset;
     }
 
     public void OnPointerDown(PointerEventData eventData)
