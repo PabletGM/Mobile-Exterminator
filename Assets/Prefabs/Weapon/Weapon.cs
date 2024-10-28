c5aeb3591f20b6f67b17a443f9907598214afd4b
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-10-28 21:42:19 +0100

    The Rifle has heritance from weapon and the ttack point is exact with Rifle and Player

---
+++ Assets/Prefabs/Weapon/Weapon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//parent class that all the weapons will have
+public  abstract class Weapon : MonoBehaviour
+{
+   public GameObject Owner
+   {
+        get;
+        private set;
+   }
+
+    //To initialise as the owner a GameObject
+   public void Init(GameObject owner)
+   {
+        Owner = owner;
+   }
+
+   public void Equip()
+   {
+        gameObject.SetActive(true);
+   }
+
+    public void UnEquip()
+    {
+        gameObject.SetActive(false);
+    }
+}
