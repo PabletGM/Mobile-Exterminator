9d29a2760734bd18d04696698f652e9f8f6adcfe
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-10-30 16:37:13 +0100

    Health component script

---
+++ Assets/Prefabs/Framework/HealthComponent.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public delegate void OnHealthChange(float health, float amount, float maxHealth);
+    public delegate void OnTakeDamage(float health, float amount, float maxHealth);
+    public delegate void OnHealthEmpty();
+
+    [SerializeField] float health = 100;
+    [SerializeField] float maxhealth = 100;
+
+    public event OnHealthChange onHealthChange;
+    public event OnTakeDamage onTakeDamage;
+    public event OnHealthEmpty onHealthEmpty;
+
+    public void changeHealth(float amount)
+    {
+        //no damage
+        if(amount == 0)
+        {
+            return;
+        }
+
+        //-damage or + health regeneration
+        health += amount;
+
+        //if there is damage
+        if(amount < 0)
+        {
+            onTakeDamage?.Invoke(health, amount, maxhealth);
+        }
+        //if there is regenerate health
+        else
+        {
+            onHealthChange?.Invoke(health, amount, maxhealth);
+        }
+
+        //if there is no health
+        if(health <= 0)
+        {
+            onHealthEmpty?.Invoke();
+        }
+    }
+}
