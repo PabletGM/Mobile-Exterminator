7ea377e1975917c1cf6225346e864a6ec0eea572
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-10-31 14:39:59 +0100

    The enemy "Remembers" that It has seen you for 3 secs
    Once the enemy has detected player with Sense or Sight lets give  a delay till the enemy forgets it has seen you.
    
    It is used the dictionary.

--- Assets/Prefabs/Framework/AI/Perception/SenseComponent.cs
+++ Assets/Prefabs/Framework/AI/Perception/SenseComponent.cs
@@ -10,6 +10,13 @@ public abstract class SenseComponent : MonoBehaviour
     static List<PerceptionStimuli> registeredStimulis = new List<PerceptionStimuli>();
     //list of perceived stimulis
     List<PerceptionStimuli> PerceivableStimuli = new List<PerceptionStimuli>();
+    //creates a dictionary that serves as a data structure for managing active forgetting routines associated with each PerceptionStimuli object.
+    //ForgettingRoutines: This is the dictionary where keys are PerceptionStimuli and values are Coroutine objects.
+    Dictionary<PerceptionStimuli, Coroutine> ForgettingRoutines = new Dictionary<PerceptionStimuli, Coroutine>();
+    
+    [SerializeField]
+    float forgettingTime = 3f;
+
     static public void RegisterStimuli(PerceptionStimuli stimuli)
     {
         if(registeredStimulis.Contains(stimuli))
@@ -20,31 +27,50 @@ public abstract class SenseComponent : MonoBehaviour
         registeredStimulis.Add(stimuli);
     }
 
+
     static public void UnRegisterStimuli(PerceptionStimuli stimuli)
     {
         registeredStimulis.Remove(stimuli);
     }
 
+
+    //=======================================================================================================
+   
+
+    //an obligatory method that their children will have
     protected abstract bool IsStimuliSensable(PerceptionStimuli stimuli);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+
+    //=========================================================================================================
+
 
     // Update is called once per frame
     void Update()
     {
         foreach(var stimuli in registeredStimulis)
         {
-            //is the stimuli being sensed
+            //is the stimuli being sensed, ask the children senses, if yes
             if(IsStimuliSensable(stimuli))
             {
                 //if it has not been perceived
-                if(!PerceivableStimuli.Contains(stimuli))
+                if (!PerceivableStimuli.Contains(stimuli))
                 {
                     PerceivableStimuli.Add(stimuli);
-                    Debug.Log($"I just sensed {stimuli.gameObject}");
+                    //if it has been an iniciated coroutine of Forgetting and we are perceived again, we stop that coroutine
+                    //This method attempts to retrieve(recuperar) the value associated with the key stimuli from the ForgettingRoutines dictionary.
+                    //This method returns true if the key exists in the dictionary and retrieves the associated value(coroutine). If the key does not exist, it returns false.
+                    if (ForgettingRoutines.TryGetValue(stimuli, out Coroutine routine))
+                    {
+                        //stops coroutine
+                        StopCoroutine(routine);
+                        //removes the routine from the dictionary
+                        ForgettingRoutines.Remove(stimuli);
+                    }
+                    else
+                    {
+                        Debug.Log($"I just sensed {stimuli.gameObject}");
+                    }
+                   
                 }
             }
             //not being sensed
@@ -53,9 +79,10 @@ public abstract class SenseComponent : MonoBehaviour
                 //BUT perceived
                 if(PerceivableStimuli.Contains(stimuli))
                 {
-                    //It was an error on detection or it is not currently sensing it
                     PerceivableStimuli.Remove(stimuli);
-                    Debug.Log($"I lost track of sensed {stimuli.gameObject}");
+                    //adding to the dictionary the stimuli and the corroutine forget.
+                    ForgettingRoutines.Add(stimuli, StartCoroutine(ForgetStimuli(stimuli)));
+
                 }
             }
         }
@@ -69,5 +96,19 @@ public abstract class SenseComponent : MonoBehaviour
     private void OnDrawGizmos()
     {
         DrawDebug();
+    }
+
+
+    //================================================================================================
+
+    
+    // Coroutine to forget a stimulus after 5 seconds
+    public IEnumerator ForgetStimuli(PerceptionStimuli stimuli)
+    {
+        yield return new WaitForSeconds(forgettingTime); // Waits for 5 seconds
+        
+        ForgettingRoutines.Remove(stimuli);    
+
+        Debug.Log($"I lost track of sensed {stimuli.gameObject}");
     }
 }
