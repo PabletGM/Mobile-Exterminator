cb3e837baddf991d0cf37c4932ca584df891c688
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-11-04 16:50:51 +0100

    Compositor c#, there are 2 types, Selector and Sequencer
    -Selector: Choose the first one node correct from left to right and it leaves
    -Sequence: We will make all the nodes while they are correct, if it is one incorrect, we leave. if all the nodes are correct the sequence is succesful, else is wrong

---
+++ Assets/Prefabs/Framework/AI/BehaviourTree/Compositor/Compositor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//collection of children nodes
+public abstract class Compositor : BTNode
+{
+    //list with all the nodes
+   LinkedList<BTNode> children = new LinkedList<BTNode>();
+    //the current node we are workig on
+   LinkedListNode<BTNode> currentChild = null;
+
+    protected override NodeResult Execute()
+    {
+        //if there is not any node
+        if(children.Count == 0)
+        {
+            return NodeResult.Success;
+        }
+
+        currentChild = children.First;
+        return NodeResult.InProgress;
+    }
+
+    //to go to next node on children LinkedList
+    protected bool Next()
+    {
+        if(currentChild != children.Last)
+        {
+            currentChild = currentChild.Next;
+            return true;
+        }
+        //if last node
+        return false;
+    }
+
+    protected override void End()
+    {
+        currentChild = null;
+    }
+}
