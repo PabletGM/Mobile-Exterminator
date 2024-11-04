68ee7171b61ee10769f4da211384c45753ef9809
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-11-04 15:45:18 +0100

    Parent Class BTNode creation

---
+++ Assets/Prefabs/Framework/AI/BehaviourTree/BTNode.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeResult
+{
+    Success,
+    Failure,
+    InProgress
+}
+public abstract class BTNode
+{
+   public NodeResult UpdateNode()
+   {
+        //one off thing
+        if(!started)
+        {
+            started = true;
+            //ask the result of the node
+            NodeResult executeResult = Execute();
+            //if it is not in Progress we finish it
+            if(executeResult != NodeResult.InProgress)
+            {
+                EndNode();
+                return executeResult;
+            }
+        }
+
+        //time based
+        NodeResult updateResult = Update();
+        if(updateResult != NodeResult.InProgress)
+        {
+            EndNode();
+        }
+        return updateResult;
+   }
+
+    //==============================================================================================================================================================
+
+
+    protected virtual NodeResult Update()
+    {
+        //time based
+        return NodeResult.Success;
+    }
+
+    //override in child class
+    protected virtual NodeResult Execute()
+    {
+        //one off thing
+        return NodeResult.Success;
+    }
+
+    private void EndNode()
+    {
+        started = false;
+        End();
+    }
+
+    protected virtual void End()
+    {
+        //clean up on Child Class
+    }
+
+    bool started = false;
+}
