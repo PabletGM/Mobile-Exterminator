8f3ae4da6639ab448b5bdf7acd284cc1729ed22b
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-11-19 22:33:14 +0100

    Spitter prefab works as the chomper with different acceptableDistance

--- Assets/Prefabs/Enemy/Spitter/SpitterBehaviour.cs
+++ Assets/Prefabs/Enemy/Spitter/SpitterBehaviour.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpitterBehaviour : MonoBehaviour
+//same behaviour of the chomper excepting the acceptableDistance = 5
+public class SpitterBehaviour : BehaviourTree
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void ConstructTree(out BTNode rootNode)
     {
-        
+        //create a selector
+        Selector rootSelector = new Selector();
+
+        #region Attack
+
+        //creation of the attackTarget group
+        BTTaskGroup_AttackTarget attackTargetGroupTask = new BTTaskGroup_AttackTarget(this, 5, 10f);
+        //added as child
+        rootSelector.AddChild(attackTargetGroupTask);
+
+        #endregion
+
+        #region CheckLastSeenLocationSequencer
+
+        //creation of the  CheckLastSeenLocation group
+        BTTaskGroup_MoveToLastSeenLocation CheckLastSeenLocation = new BTTaskGroup_MoveToLastSeenLocation(this, 1f);
+        //added as child
+        rootSelector.AddChild(CheckLastSeenLocation);
+
+        #endregion
+
+        #region Patrolling
+
+        //creation of the  CheckLastSeenLocation group
+        BTTaskGroup_Patrolling patrol = new BTTaskGroup_Patrolling(this, 1f);
+        //added as child
+        rootSelector.AddChild(patrol);
+
+        #endregion
+
+        //rootNode
+        rootNode = rootSelector;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
