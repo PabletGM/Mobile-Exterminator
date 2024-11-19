4657226f854f3fe00de052d1208ddd316108587a
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-11-19 22:20:57 +0100

    Spitter prefab, Spitter class and SpitterBehaviour class

---
+++ Assets/Prefabs/Enemy/Spitter/Spitter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spitter : Enemy
+{
+    [SerializeField] TriggerDamageComponent triggerDamageComp;
+    //override of the interface  IBehaviourTree method
+    public override void AttackTarget(GameObject target)
+    {
+        Animator.SetTrigger("Attack");
+    }
+
+    //animation event on chomper attack melee
+    public void AttackPoint()
+    {
+        //give permission to start the attack
+        if (triggerDamageComp)
+        {
+            triggerDamageComp.SetDamageEnabled(true);
+        }
+    }
+
+    public void AttackEnd()
+    {
+        //give permission to start the attack
+        if (triggerDamageComp)
+        {
+            triggerDamageComp.SetDamageEnabled(false);
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        //to initialize the team interface of the DamageComponent
+        //triggerDamageComponent and DamageComponent are parent and son so we can call the methods of DamageComp
+        triggerDamageComp.SetTeamInterfaceSource(this);
+    }
+}
